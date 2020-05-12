f98de67a2a6639bd20d9fee9182a8f2b9210a111
Martynas Kasparavičius <martis.kasparavicius@gmail.com>
2020-05-12 21:50:53 +0300

    Date format fix

--- server/server/Migrations/20200427160949_TransactionFixtures.cs
+++ server/server/Migrations/20200427160949_TransactionFixtures.cs
@@ -12,10 +12,10 @@ namespace server.Migrations
                 columns: new[] { "Id", "AccountId", "Amount", "Category", "Date", "Description", "Disabled" },
                 values: new object[,]
                 {
-                    { 1, 1, 125.31, 6, DateTime.Now, "Some description", false },
-                    { 2, 1, 13.99, 8, DateTime.Now, null, false },
-                    { 3, 1, 4.65, 0, DateTime.Now, "McDonalds", true },
-                    { 4, 3, 87.43, 7, DateTime.Now, null, false }
+                    { 1, 1, 125.31, 6, DateTime.UtcNow, "Some description", false },
+                    { 2, 1, 13.99, 8, DateTime.UtcNow, null, false },
+                    { 3, 1, 4.65, 0, DateTime.UtcNow, "McDonalds", true },
+                    { 4, 3, 87.43, 7, DateTime.UtcNow, null, false }
                 });
         }
 
