a0f400163493534c81d2d0fd25010c6b1224979b
unknown <mykolasvitkus@gmail.com>
2020-05-25 23:59:54 +0300

    hehe

---
+++ server/server/ResourceManagers/BudgetManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using server.Models;
+using server.Services;
+
+namespace server.ResourceManagers
+{
+    public class BudgetManager : IBudgetManager
+    {
+        private readonly IStorage<Budget> _budgetStorage;
+
+        public BudgetManager(IStorage<Budget> budgetStorage)
+        {
+            _budgetStorage = budgetStorage;
+        }
+        public Budget GetBudget(int id) 
+        {
+            return _budgetStorage.getItem(id);
+        }
+        public Budget AddBudget(Budget budget) 
+        {
+            return _budgetStorage.createItem(budget);
+        }
+        public IQueryable<Budget> GetBudgets(int ?userId)
+        {
+            return _budgetStorage.getCollection();
+        }
+        public void DeleteBudget(Budget budget) { }
+    }
+}
---
+++ server/server/ResourceManagers/IBudgetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using server.Models;
+
+namespace server.ResourceManagers
+{
+    public interface IBudgetManager
+    {
+        Budget GetBudget(int id);
+        Budget AddBudget(Budget budget);
+        IQueryable<Budget> GetBudgets(int ?userId);
+        void DeleteBudget(Budget budget);
+
+    }
+}
